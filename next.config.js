5cd4ac2ece54fa5ceea52ba03fc572c9d225c0a3
Mercury-nju <2945235656@qq.com>
2025-10-29 11:58:51 +0800

    fix: simplify next.config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -2,20 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**',
       },
     ],
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.ico$/,
-      type: 'asset/resource',
-    });
-    return config;
   },
 }
 
